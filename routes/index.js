8d64ebc54828a5375484b29f7fc82204e87447ca
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2022-10-22 22:08:55 +0300

    Добавил страницы кратоса и артрея

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/kratos_old', function(req, res, next) {
+  res.render('index', {title: "<h1>Страница Кратоса - убийцы богов! </h1>"});
+});
+
+router.get('/kratos_new', function(req, res, next) {
+  res.render('index', {title: "<h1>Страница Кратоса - отца </h1>"});
+});
+
+router.get('/Artrey', function(req, res, next) {
+  res.render('index', {title: "<h1>Страница Артрея - сына Кратоса </h1>"});
+});
+
 module.exports = router;
