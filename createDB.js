2b55a1c83fe9e62dbe482a4f9797b2dc825dc1c8
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2022-10-24 16:29:30 +0300

    Поставил БД

---
+++ createDB.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient
+
+const uri = "mongodb://localhost:27017/"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("Kratos");
+database.dropDatabase()
+database = client.db("Kratos");
+const cats = database.collection("GoW");
+const result = await cats.insertOne({name:"Kratos"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run()
