70a56cb968944490957959c6991df683b3775834
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2022-10-28 21:51:47 +0300

    Добавил счетчик посещений

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ router.get('/', function(req, res, next) {
         req.session.greeting = "Hi!!!",
         res.cookie('greeting','Hi!!!').render('index', {
                                 title: 'Express',
-                                menu: menu
+                                menu: menu,
+                                counter: req.session.counter,
                             });
                             
     })
