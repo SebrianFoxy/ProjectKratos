459b028af3ddff39e4c6e921b42d86a9e0995942
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-01-07 16:49:05 +0300

    Заполнение меню из базы

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,13 +1,11 @@
-var God = require("./../models/god").God
-
+var db = require('../mySQLConnect.js');
 
 module.exports = function(req,res,next){
-    res.locals.nav = []
-
-    God.find(null,{_id:0,title:1,nick:1},function(err,result){
+res.locals.nav = []
+    db.query(`SELECT * FROM gods`, (err, gods) => {  
         if(err) throw err
-        res.locals.nav = result
+        res.locals.nav = gods
         next()
+        
     })
 }
-
