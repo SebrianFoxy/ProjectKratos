8587df8b9d9bba6b919b6e626696aeb66822aeec
SebrianFoxy <bogdan.medvedewstudy@gmail.com>
2023-01-07 14:23:10 +0300

    Вывод данных на экранах

--- routes/gods.js
+++ routes/gods.js
@@ -1,39 +1,34 @@
-
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 var db = require('../mySQLConnect.js');
-//var God = require("../models/god").God
 //var checkAuth = require("./../middleware/checkAuth.js")
 //var async = require("async")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('Новый маршрутизатор, для маршрутов, начинающихся с gods')
+    res.send('Новый маршрутизатор, для маршрутов, начинающихся с gods');
 });
 
-
-
-
-/* Страница Богов! */
-router.get('/:nick', function(req, res, next) {
-    //God.findOne({nick:req.params.nick}, function(err,god){ })
+/* Страница ведущих */
+router.get('/:nick' , function(req, res, next) {
     db.query(`SELECT * FROM gods WHERE gods.nick = '${req.params.nick}'`, (err, gods) => {
-    if(err) {
-    console.log(err);
-    if(err) return next(err)
-    }else {
+        if(err) {
+        console.log(err);
         if(err) return next(err)
-        if(gods.lenght == 0) return next(new Error("Нет такого героя для поиска"))
-        var god = gods[0];
-        res.render('god', {
-            title: god.title,
-            picture: god.avatar,
-            desc: god.about
-            })
-        }
+        }else {
+            if(err) return next(err)
+            if(gods.lenght == 0) return next(new Error("Нет такого героя для поиска"))
+            var god = gods[0];
+            res.render('god', {
+                title: god.title,
+                picture: god.avatar,
+                desc: god.about
+                })
+            }
+        })
     })
-})
+    
 
 
 
-module.exports = router
+module.exports = router;
